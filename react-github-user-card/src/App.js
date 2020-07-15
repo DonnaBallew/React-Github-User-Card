b924694398bced5c32a6c993841bbb4cc7b26f4e
Donna Ballew <ballewdd@gmail.com>
2020-07-14 21:44:30 -0700

    re-worked App.js

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,35 +1,15 @@
 import React from "react";
-
-import UserCard from "./components/UserCard";
-import UserFollowers from "./components/UserFollowers";
-
 import "./App.css";
+import UserCard from "./components/UserCard";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-    };
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Github User Card</h1>
 
-  componentDidMount() {
-    fetch("https://api.github.com/users/DonnaBallew")
-      .then((res) => res.json())
-      .then((res) => this.setState({ users: res }))
-      .catch((err) => console.log("err"));
-  }
-  // https://www.smashingmagazine.com/2020/06/rest-api-react-fetch-axios/
-  render() {
-    return (
-      <div className="App">
-        <h1>Github User Card</h1>
-        {/* {console.log(this.state.users)} */}
-        <UserCard users={this.state.users} />
-        <UserFollowers />
-      </div>
-    );
-  }
+      <UserCard />
+    </div>
+  );
 }
 
 export default App;
