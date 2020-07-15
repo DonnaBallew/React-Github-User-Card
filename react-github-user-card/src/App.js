2232a4ff0a5cf18b7a0210869c9d70303457e60a
Donna Ballew <ballewdd@gmail.com>
2020-07-14 17:56:29 -0700

    build out App.js; not complete

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,16 +1,32 @@
 import React from "react";
 
+import UserCard from "./components/UserCard";
+import UserFollowers from "./components/UserFollowers";
+
 import "./App.css";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      users: [],
+    };
   }
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/DonnaBallew")
+      .then((res) => res.json())
+      .then((res) => this.setState({ users: res }))
+      .catch((err) => console.log("err"));
+  }
+  // https://www.smashingmagazine.com/2020/06/rest-api-react-fetch-axios/
   render() {
     return (
       <div className="App">
-        <h1>GitHub User Card</h1>
+        <h1>Github User Card</h1>
+        {/* {console.log(this.state.users)} */}
+        <UserCard users={this.state.users} />
+        <UserFollowers />
       </div>
     );
   }
