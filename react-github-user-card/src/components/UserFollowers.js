e21dc9d04e23dcaca75156c6d682918a6624f682
Donna Ballew <ballewdd@gmail.com>
2020-07-14 18:47:36 -0700

    added dependencies and keys

---
+++ react-github-user-card/src/components/UserFollowers.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
+
+class UserFollowers extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      followers: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/DonnaBallew/followers")
+      .then((res) => res.json())
+      .then((res) => this.setState({ followers: res }))
+      .catch((err) => console.log("err"));
+  }
+  render() {
+    return (
+      <div>
+        {console.log(this.state.followers)}
+        {this.state.followers.map((follower) => {
+          return (
+            <Card key={follower.id}>
+              <Image src={follower.avatar_url} wrapped ui={false} />
+              <Card.Content>
+                <Card.Header>Github:@{follower.login}</Card.Header>
+              </Card.Content>
+              <Card.Content extra></Card.Content>
+            </Card>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default UserFollowers;
